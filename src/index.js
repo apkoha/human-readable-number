0e087e0046351201348498e6e77a51c5e035c8b7
apkoha <russiapatriot2@yandex.ru>
2024-08-12 08:36:14 +0300

    feat: function complete (Mon, Aug 12, 2024 08:35:24 AM)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function toReadable (number) {
-  
-}
+  const uniqueNumbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",  "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+  const tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+  if (number < 20) {
+    return uniqueNumbers[number];
+  };
+
+  if (number < 100) {
+    //получаем округлением к меньшему номер десятка.
+    const tensNumber = Math.floor(number / 10);
+    //второе число - остаток от деления на 10;
+    const secondNumber = number % 10;
+
+    //возвращаем конкатенацию. десяток - номер элемента в массиве tens плюс число - номер элемента в массиве uniqueNumbers.
+    return tens[tensNumber - 2] + (secondNumber ? " " + uniqueNumbers[secondNumber] : "");
+  };
+
+  if (number < 1000) {
+    //получаем округлением к меньшему номер сотни.
+    const hundredsNumber = Math.floor(number / 100);
+    //второе число - остаток от деления на 100;
+    const balance = number % 100;
+    
+    //конкатенируем номер сотни из массива uniqueNumbers со словом " hundred"
+    //добавляем реузультат выполнения функции toReadable() для двухзначного числа balance
+    return uniqueNumbers[hundredsNumber] + " hundred" + (balance ? " " + toReadable(balance) : "");
+  };
+};
